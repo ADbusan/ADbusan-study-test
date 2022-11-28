a5507b84878693cb6df2097d04cf45549875087c
ADbusan <dlsnditk90@naver.com>
2022-11-28 15:33:03 +0900

    java 백준 문제2

--- jh_ex2/jh_05.java
+++ jh_ex2/jh_05.java
@@ -8,15 +8,14 @@ public class jh_05 {
         System.out.println("시간을 입력");
         int time =scan.nextInt();
         System.out.println("분을 입력");
-        int time2= scan.nextInt();
+        int time2=scan.nextInt();
 
-        if (time2<45){
-            time--;
-            if (time<0){
-                time = 23;
-                System.out.println(time+":"+(time2+60-45));
-            }
-        }
+        if (time2>44){
+            int num = time2-45;
+            System.out.println(time+":"+num);
+        }else if (time2<45){
+            int num1 = (60+time2)-45;
+            System.out.println(time-1+":"+num1);
         }
     }
-
+}
