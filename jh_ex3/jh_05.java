bf4252034fbf8d935c5ed9815006a7a8df5b707c
ADbusan <dlsnditk90@naver.com>
2022-11-28 15:33:15 +0900

    java 백준 문제3

--- jh_ex3/jh_05.java
+++ jh_ex3/jh_05.java
@@ -1,22 +1,52 @@
 package jh_ex3;
 
 import java.util.Scanner;
-
 public class jh_05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("첫 숫자를 입력하시오: ");
         int n = scan.nextInt();
-//        n이 26이면
-//        십의 자리 = 2
-//        일의 자리 = 6
-//        60+
         int temp = n;
         int count = 0;
         while (true) {
-            int left = temp / 10;//십의 자리
-            int right = temp % 10;//일의 자리
-
+            int left = temp / 10;// a의 십의 자리
+            int right = temp % 10;// a의 일의 자리
+            int d = left+right; // d는 8
+            int e =d%10;
+            temp = right * 10 + e;// 68 d=left+right
+            count++;
+            if (temp == n) {
+                break;
+            }
         }
+        System.out.println(temp);
+        System.out.println(count);
     }
 }
+//26
+//left = 2
+//right = 6
+//d = 8
+//right *10 =60
+//d % 10 = 8
+//right % 10 +d % 10=68
+
+//68
+//left = 6
+//right = 8
+//d = 14
+//right * 10 = 80
+//d % 10 = 4
+//right % 10 +d % 10=84
+
+//84
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
